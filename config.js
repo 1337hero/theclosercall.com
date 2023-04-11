a65fcd2b0e3e422368d0003e87aa069c1f7cb6ed
Mike Key <mike@1337hero.com>
2023-04-11 11:58:17 -0600

    initial build out

--- config.js
+++ config.js
@@ -1,22 +1,21 @@
 module.exports = {
-    "siteTitle": "Mike Key | Content Creator & Real Estate Investor",
+    "siteTitle": "The Closer Call",
     "author": "Mike Key",
-    "description": "Hey 👋 I am Mike Key. Content Creator & Real Estate Investor. Use this site to connect with me.",
+    "description": "",
 
     /*-- Intro --*/
-    "avatar": "./img/mike.webp",
-    "pageTitle": "Mike Key",
-    "subTitle": "Content Creator & RE Investor",
-    "tagline": "I help YOU create engaging content & build a personal brand",
+    "pageTitle": "Release your potential",
+    "subTitle": "... need to come up with something?",
+    "heroCTA": "Get Started",
 
     /*-- Social Media Links --*/
-    "youtube": "https://www.youtube.com/@justmikekey",
-    "instagram": "https://www.instagram.com/justmikekey/",
-    "facebook": "https://www.facebook.com/justmikekey",
-    "linkedin": "https://www.linkedin.com/in/1337hero/",
-    "twiiter": "https://twitter.com/justmikekey",
-    "tiktok": "https://www.tiktok.com/@justmikekey",
+    "youtube": "",
+    "instagram": "",
+    "facebook": "",
+    "linkedin": "",
+    "twiiter": "",
+    "tiktok": "",
 
     /*-- Connect Buttons --*/
-    "calendly": "https://calendly.com/justmikekey/connect-with-mike"
+    "calendly": ""
 }
