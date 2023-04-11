784b0887914ddeed111764353134e4856c916d9d
Mike Key <mike@1337hero.com>
2023-04-11 08:20:04 -0600

    irst commit

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "siteTitle": "Mike Key | Content Creator & Real Estate Investor",
+    "author": "Mike Key",
+    "description": "Hey 👋 I am Mike Key. Content Creator & Real Estate Investor. Use this site to connect with me.",
+
+    /*-- Intro --*/
+    "avatar": "./img/mike.webp",
+    "pageTitle": "Mike Key",
+    "subTitle": "Content Creator & RE Investor",
+    "tagline": "I help YOU create engaging content & build a personal brand",
+
+    /*-- Social Media Links --*/
+    "youtube": "https://www.youtube.com/@justmikekey",
+    "instagram": "https://www.instagram.com/justmikekey/",
+    "facebook": "https://www.facebook.com/justmikekey",
+    "linkedin": "https://www.linkedin.com/in/1337hero/",
+    "twiiter": "https://twitter.com/justmikekey",
+    "tiktok": "https://www.tiktok.com/@justmikekey",
+
+    /*-- Connect Buttons --*/
+    "calendly": "https://calendly.com/justmikekey/connect-with-mike"
+}
