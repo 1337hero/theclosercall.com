2ecc1081e5700b8539a10c022bb4f23e96b4e609
Mike Key <mike@1337hero.com>
2023-04-11 16:03:07 -0600

    major revisions, approaching launch

--- config.js
+++ config.js
@@ -1,12 +1,37 @@
 module.exports = {
     "siteTitle": "The Closer Call",
-    "author": "Mike Key",
+    "author": "1337 Hero",
     "description": "",
 
-    /*-- Intro --*/
+    /*-- Site Banner --*/
     "pageTitle": "Release your potential",
     "subTitle": "... need to come up with something?",
     "heroCTA": "Get Started",
+    "heroIMG": "url('/img/hero-img-bw.webp')",
+
+    /*-- Video Section --*/
+    "videoTitle": "Ready to close more deals?",
+    "videoParagraph": "Jump into weekly calls where you will be able to be a part of amazing conversations that will level up your closing skills!",
+    "mp4source": "https://s3.amazonaws.com/media.mikekey.com/DQ.webm",
+    "webmsource": "https://s3.amazonaws.com/media.mikekey.com/DQ.mp4",
+
+    /*-- Pricing --*/
+    "pricingTitle": "Become a Member",
+    "pricingParagraph": "Members enjoy exclusive benefits and access. Choose the membership level that’s right for you.",
+    
+    "boxOneTier": "Silver",
+    "boxOnePrice": "150",
+    "boxOneLink": "https://sales.theclosercall.com/entry-package",
+
+    "boxTwoTier": "Gold",
+    "boxTwoPrice": "250",
+    "boxTwoLink": "https://sales.theclosercall.com/premium-package",
+
+    "boxThreeTier": "Custom",
+    "boxThreePrice": "Call for Pricing",
+    "boxThreeLink": "https://bit.ly/OneonOnewithDQ",
+
+    "priceCTA": "Choose",
 
     /*-- Social Media Links --*/
     "youtube": "https://www.youtube.com/@Daniel.TheQOfficial",
@@ -15,6 +40,6 @@ module.exports = {
     "tiktok": "https://www.tiktok.com/@daniel.theq",
 
 
-    /*-- Connect Buttons --*/
-    "calendly": ""
+ 
+
 }
